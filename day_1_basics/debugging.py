127f27c6abaa681ba5b6ccd871bd93e48c28a7ec
ajaytech002 <siva@ajaytech.co>
2019-05-27 00:41:45 +0530

    files

--- day_1_basics/debugging.py
+++ day_1_basics/debugging.py
@@ -8,4 +8,4 @@ def calculate_interest(balance,interest,days) :
     interest_amount = balance * ( interest / 100 ) * ( days/365 )
     return interest_amount
 
-calculate_interest
+calculate_interest.
