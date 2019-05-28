47ea44f8cd4effdccee59dccef5014c238608e61
ajaytech002 <siva@ajaytech.co>
2019-05-28 23:59:19 +0530

    statistics

--- day_1_basics/debugging.py
+++ day_1_basics/debugging.py
@@ -1,11 +1,4 @@
-def calculate_interest(balance,interest,days) :
-    """
-    Calculates interest on a given balance. The key arguments are
-    1. Balance  - Amount on which balance needs to be calculated
-    2. Interest - Annual interest in percentage
-    3. Days     - Number of days since the beginning of the year
-    """
-    interest_amount = balance * ( interest / 100 ) * ( days/365 )
-    return interest_amount
+sum = 0
 
-calculate_interest.
+for num in range(10000000) :
+    sum = sum + num
