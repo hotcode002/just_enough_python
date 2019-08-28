ee3ce2a726ac79ece156a20f640279bbdf6615b9
ajaytech002 <siva@ajaytech.co>
2019-08-28 12:51:24 +0530

    debugging

--- day_1_basics/debugging.py
+++ day_1_basics/debugging.py
@@ -1,4 +1,13 @@
-sum = 0
-
-for num in range(10000000) :
-    sum = sum + num
+first_num = int (input ("first num >"))
+second_num = int (input ("second num >"))
+for number in range(first_num,second_num + 1):
+    
+    found_prime = True
+    for i in range(2, number-1):
+        if number % i == 0 :
+            print ( number , "is not a prime number")
+            found_prime = False
+            break
+            
+    if found_prime == True :
+        print ( number , "is a prime number")
